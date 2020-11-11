0c3ca999d7e6f1d7598b27f0d805c04707597bc1
TianyouLi <litianyou97@gmail.com>
2020-11-10 21:57:04 -0500

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2404979019809327',
+        clientSecret: '45df5a8e214b5b05cee84e23e53ecf92'
+    }
 };
